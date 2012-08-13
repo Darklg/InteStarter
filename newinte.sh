437730c513ea226736bd1cbc955dcf88c41f138c
Darklg <kev.rocher@gmail.com>
2012-08-13 14:19:55 +0200

    Deploiement automatique des bases de CSSNormalize

--- newinte.sh
+++ newinte.sh
@@ -7,6 +7,24 @@ git clone git@github.com:Darklg/InteStarter.git
 mv InteStarter inte
 cd inte/
 
+# On y clone CSSNormalize
+git clone git@github.com:Darklg/CSSNormalize.git
+
+# On installe les feuilles de style
+cp CSSNormalize/css/reset.css css/reset.css
+cp CSSNormalize/css/normalize.css css/normalize.css
+cp CSSNormalize/css/normalize-common.css css/normalize-common.css
+cp CSSNormalize/css/normalize-base.css css/normalize-base.css
+
+echo "
+@import 'reset.css';
+@import 'normalize.css';
+@import 'normalize-common.css';
+@import 'normalize-base.css';" >> css/zz-all.css
+
+# On supprime CSSNormalize
+rm -rf CSSNormalize
+
 # Suppression des fichiers inutiles & de développement
 rm -rf .git
 rm README.md
