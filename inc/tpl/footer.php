67ee89e36849029764f65def43faa0adac6645e9
Darklg <kev.rocher@gmail.com>
2013-06-13 20:16:25 +0200

    Centered container on header & footer

--- inc/tpl/footer.php
+++ inc/tpl/footer.php
@@ -1,4 +1,4 @@
-<footer id="footer">
+<footer id="footer" class="centered-container">
     <?php include dirname(__FILE__).'/footer/copyright.php'; ?>
 </footer>
 </body>
--- inc/tpl/header.php
+++ inc/tpl/header.php
@@ -8,6 +8,6 @@
 <?php include dirname(__FILE__).'/header/head.php'; ?>
 </head>
 <body>
-    <header id="header">
         <h1><a href="#"><?php echo PROJECT_NAME; ?></a></h1>
+    <header id="header" class="centered-container">
     </header>
