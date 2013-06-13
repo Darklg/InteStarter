5928ad10cf505773c51d037b549c68590b6d4a28
Darklg <kev.rocher@gmail.com>
2013-06-13 20:15:54 +0200

    Removing IE6 support

--- inc/tpl/header.php
+++ inc/tpl/header.php
@@ -1,9 +1,8 @@
 <!DOCTYPE HTML>
-<!--[if lt IE 7 ]><html lang="fr-FR" class="is_ie6 lt_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]--> 
-<!--[if IE 7 ]><html lang="fr-FR" class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]--> 
-<!--[if IE 8 ]><html lang="fr-FR" class="is_ie8 lt_ie9 lt_ie10"><![endif]--> 
-<!--[if IE 9 ]><html lang="fr-FR" class="is_ie9 lt_ie10"><![endif]--> 
-<!--[if gt IE 9]><html lang="fr-FR" class="is_ie10"><![endif]--> 
+<!--[if lt IE 8 ]><html lang="fr-FR" class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]-->
+<!--[if IE 8 ]><html lang="fr-FR" class="is_ie8 lt_ie9 lt_ie10"><![endif]-->
+<!--[if IE 9 ]><html lang="fr-FR" class="is_ie9 lt_ie10"><![endif]-->
+<!--[if gt IE 9]><html lang="fr-FR" class="is_ie10"><![endif]-->
 <!--[if !IE]><!--> <html lang="fr-FR"><!--<![endif]-->
 <head>
 <?php include dirname(__FILE__).'/header/head.php'; ?>
