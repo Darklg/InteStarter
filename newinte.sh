0d9983408ff553656230a5788098a0265adddb9a
Darklg <kev.rocher@gmail.com>
2014-06-11 12:22:20 +0200

    Comments & fixes

--- newinte.sh
+++ newinte.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # On clone le repository
-echo '# RECUPERATION DE INTESTARTER'
+echo '# - RECUPERATION DE INTESTARTER'
 git clone git://github.com/Darklg/InteStarter.git
 
+#################################################################
+## CONFIGURATION INITIALE
+#################################################################
 
-read -p "# - Utiliser un sous-dossier (y/n) ? " use_subfolder
+echo '## CONFIGURATION INITIALE';
+
+# Choix du dossier
+read -p "- Utiliser un sous-dossier (y/n) ? " use_subfolder
 case "$use_subfolder" in
     y|Y|O|o )
         # On renomme le dossier créé et on s'y déplace
@@ -20,19 +26,27 @@ case "$use_subfolder" in
 esac
 
 # On recupere le nom du projet
-read -p "# - Comment s'appelle ce projet ? " project_name
+read -p "- Comment s'appelle ce projet ? " project_name
 echo "define('PROJECT_NAME','"${project_name/\'/’}"');" >> inc/config.php
 
 # On recupere l'URL du projet
-read -p "# - Quelle est l'URL du projet ? " project_url
+read -p "- Quelle est l'URL du projet ? " project_url
 echo "define('PROJECT_URL','"${project_url/\'/’}"');" >> inc/config.php
 
 # On recupere la description du projet
-read -p "# - Quel est la description rapide du projet ? " project_description
+read -p "- Quel est la description rapide du projet ? " project_description
 echo "define('PROJECT_DESCRIPTION','"${project_description/\'/’}"');" >> inc/config.php
+
+
+#################################################################
+## CREATION DES DOSSIERS
+#################################################################
+
+echo '## CREATION DES DOSSIERS';
 
 # On cree le repertoire des assets
 if ! [ -d assets/ ]; then
+  echo '- Creation de assets/';
   mkdir assets/
 fi
 
@@ -40,26 +54,40 @@ cd assets/
 
 # On cree le répertoire contenant le CSS
 if ! [ -d css/ ]; then
+  echo '- Creation de css/';
   mkdir css/
 fi
 
 # On cree le répertoire contenant les images
 if ! [ -d images/ ]; then
+  echo '- Creation de images/';
   mkdir images/
 fi
 
 # On cree le répertoire contenant les fonts
 if ! [ -d fonts/ ]; then
+  echo '- Creation de fonts/';
   mkdir fonts/
 fi
 
 # On cree le répertoire contenant le JS
 if ! [ -d js/ ]; then
+  echo '- Creation de js/';
   mkdir js/
 fi
 
+cd ..
+
+#################################################################
+## GESTION DU CSS
+#################################################################
+
+echo '## GESTION DU CSS';
+
+cd assets/
+
 # On y clone CSSCommon
-echo '# RECUPERATION DE CSSCOMMON'
+echo '- Recuperation de CSSCommon';
 git clone git://github.com/Darklg/CSSCommon.git
 
 # On installe les feuilles de style
@@ -72,17 +100,17 @@ echo '<link rel="stylesheet" type="text/css" href="cssc-default.css" />
 <link rel="stylesheet" type="text/css" href="cssc-content.css" />
 ' >> ../inc/tpl/header/head.php;
 
-
-read -p "# - Utiliser des modules CSSCommon (y/n) ? " use_csscommon
+# Installation de modules CSS au choix
+read -p "- Utiliser des modules CSSCommon (y/n) ? " use_csscommon
 if [[ $use_csscommon == 'y' ]]; then
-    # Installation de feuilles CSS au choix
     css_sheets="buttons forms tables grid push navigation layouts tabs images print effects"
     for i in $css_sheets
     do
-        read -p "# --- Installer le module CSS "$i" (y/n)? " choice
+        read -p "-- Installer le module CSS "$i" (y/n)? " choice
         case "$choice" in
             y|O )
-                echo '# Installation de '$i
+                echo '-- Installation de '$i;
+                pwd;
                 cp CSSCommon/css/cssc-$i.css css/cssc-$i.css
                 echo '<link rel="stylesheet" type="text/css" href="cssc-'$i'.css" />' >> ../inc/tpl/header/head.php;
             ;;
@@ -94,14 +122,22 @@ fi
 # On supprime CSSCommon
 rm -rf CSSCommon
 
+cd ..
+
+#################################################################
+## GESTION DU JS
+#################################################################
+
+echo '## GESTION DU JS';
+
 # On ajoute les fichiers JS essentiels
-cd js/
+cd assets/js/
 
 # On propose de télécharger une librairie JS
-read -p "# - Utiliser Mootools, jQuery, ou aucune librairie (m/j/n) ? " choice
+read -p "- Utiliser Mootools, jQuery, ou aucune librairie (m/j/n) ? " choice
 case "$choice" in
     m|M )
-        echo '# GO MOOTOOLS'
+        echo '- Installation de MooTools';
         curl -O http://ajax.googleapis.com/ajax/libs/mootools/1.4/mootools-yui-compressed.js;
         if test -f mootools-yui-compressed.js; then
             echo '<script src="assets/js/mootools-yui-compressed.js"></script><script src="assets/js/events.js"></script>' >> ../../inc/tpl/header/head.php;
@@ -109,7 +145,7 @@ case "$choice" in
         fi
     ;;
     j|J )
-        echo '# OK POUR JQUERY'
+        echo '- Installation de jQuery';
         curl -O http://code.jquery.com/jquery.min.js;
         if test -f jquery.min.js; then
             echo '<script src="assets/js/jquery.min.js"></script><script src="assets/js/events.js"></script>' >> ../../inc/tpl/header/head.php;
@@ -117,14 +153,24 @@ case "$choice" in
         fi
     ;;
     * )
-        echo "# OK, PAS DE LIBRAIRIE JS";
+        echo '- Aucune librairie utilisée.';
         echo '<script src="assets/js/events.js"></script>' >> ../../inc/tpl/header/head.php;
         echo "(function(){})();" > events.js;
     ;;
 esac
 
+cd ../..
+
+#################################################################
+## COMPATIBILITE IE
+#################################################################
+
+echo '## COMPATIBILITE IE';
+
+cd assets/js/
+
 # On recupere html5shim
-echo '# RECUPERATION DE HTML5SHIM'
+echo '- Récupération de html5shim ( Tags HTML5 sur IE < 9 )';
 curl -O http://html5shim.googlecode.com/svn/trunk/html5.js
 cd ..
 if test -f js/html5.js; then
@@ -132,7 +178,7 @@ if test -f js/html5.js; then
 fi
 
 # On recupere selectivizr
-echo '# RECUPERATION DE SELECTIVIZR'
+echo '- Récupération de selectivizr ( Selecteurs avancés sur IE < 9 )';
 mkdir selectivizr
 cd selectivizr
 curl -O http://selectivizr.com/downloads/selectivizr-1.0.2.zip
@@ -143,19 +189,23 @@ if test -f selectivizr/selectivizr-min.js; then
     echo '<!--[if lt IE 9]><script src="assets/js/selectivizr-min.js"></script><![endif]-->' >> ../inc/tpl/header/head.php
 fi
 rm -rf selectivizr/
-
-echo '# CUSTOM';
 cd ..
 
+#################################################################
+## VIEWPORT & RESPONSIVE
+#################################################################
+
+echo '## VIEWPORT & RESPONSIVE';
+
 # Configuration du viewport
-read -p "# - Est-ce un site responsive (y/n) ? " is_responsive
+read -p "- Est-ce un site responsive (y/n) ? " is_responsive
 case "$is_responsive" in
     y|Y|O|o )
         sed -i '' 's/width=980/width=device-width/' inc/tpl/header/head.php
         sed -i '' 's/<body>/<body class="cssc-is-responsive">/' inc/tpl/header.php
     ;;
     * )
-        read -p "# - Quelle est la largeur du contenu ? " content_width
+        read -p "- Quelle est la largeur du contenu sans marges (Default:980) ? " content_width
         if [[ $content_width != '' ]]; then
             content_width_wide=$(( $content_width+40 ));
             echo 'Viewport utilisé : '$content_width_wide;
@@ -164,12 +214,16 @@ case "$is_responsive" in
     ;;
 esac
 
+#################################################################
+## MENAGE
+#################################################################
+
+echo '## MENAGE';
 
 # Suppression des fichiers inutiles & de développement
-echo '# MENAGE'
 rm -rf .git
 rm README.md
 rm newinte.sh
 rm deploy.sh
 
-echo '# LETS WORK, BABY !'
+echo '## LET’S WORK, BABY !'
