71c3467d6611d8759760514d152f6a9e4625ac17
Darklg <kev.rocher@gmail.com>
2014-06-11 11:54:24 +0200

    Removed support for IE7 :(

--- inc/tpl/header.php
+++ inc/tpl/header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE HTML>
-<!--[if lt IE 8 ]><html lang="fr-FR" class="is_ie7 lt_ie8 lt_ie9 lt_ie10"><![endif]-->
-<!--[if IE 8 ]><html lang="fr-FR" class="is_ie8 lt_ie9 lt_ie10"><![endif]-->
+<!--[if lt IE 9 ]><html lang="fr-FR" class="is_ie8 lt_ie9 lt_ie10"><![endif]-->
 <!--[if IE 9 ]><html lang="fr-FR" class="is_ie9 lt_ie10"><![endif]-->
 <!--[if gt IE 9]><html lang="fr-FR" class="is_ie10"><![endif]-->
 <!--[if !IE]><!--> <html lang="fr-FR"><!--<![endif]-->
