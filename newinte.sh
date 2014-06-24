6c278add93686b6f61d6f43c232d1d94e7ec564c
Darklg <kev.rocher@gmail.com>
2014-06-24 22:38:01 +0200

    Use initial directory to navigate between sections & JS files order

--- newinte.sh
+++ newinte.sh
@@ -10,6 +10,8 @@ git clone git://github.com/Darklg/InteStarter.git
 
 echo '## CONFIGURATION INITIALE';
 
+MAINDIR=${PWD}"/";
+
 # Choix du dossier
 read -p "- Utiliser un sous-dossier (y/n) ? " use_subfolder
 case "$use_subfolder" in
@@ -17,6 +19,7 @@ case "$use_subfolder" in
         # On renomme le dossier créé et on s'y déplace
         mv InteStarter inte
         cd inte/
+        MAINDIR=${PWD}"/";
     ;;
     * )
         # On récupère le contenu du dossier créé
@@ -27,15 +30,15 @@ esac
 
 # On recupere le nom du projet
 read -p "- Comment s'appelle ce projet ? " project_name
-echo "define('PROJECT_NAME','"${project_name/\'/’}"');" >> inc/config.php
+echo "define('PROJECT_NAME','"${project_name/\'/’}"');" >> $MAINDIR"inc/config.php";
 
 # On recupere l'URL du projet
 read -p "- Quelle est l'URL du projet ? " project_url
-echo "define('PROJECT_URL','"${project_url/\'/’}"');" >> inc/config.php
+echo "define('PROJECT_URL','"${project_url/\'/’}"');" >> $MAINDIR"inc/config.php";
 
 # On recupere la description du projet
 read -p "- Quel est la description rapide du projet ? " project_description
-echo "define('PROJECT_DESCRIPTION','"${project_description/\'/’}"');" >> inc/config.php
+echo "define('PROJECT_DESCRIPTION','"${project_description/\'/’}"');" >> $MAINDIR"inc/config.php";
 
 
 #################################################################
@@ -75,6 +78,10 @@ if ! [ -d js/ ]; then
   echo '- Creation de js/';
   mkdir js/
 fi
+if ! [ -d js/ie/ ]; then
+  echo '- Creation de js/ie/';
+  mkdir js/ie/
+fi
 
 cd ..
 
@@ -95,10 +102,10 @@ cp CSSCommon/css/cssc-default.css css/cssc-default.css
 cp CSSCommon/css/cssc-common.css css/cssc-common.css
 cp CSSCommon/css/cssc-content.css css/cssc-content.css
 
-echo '<link rel="stylesheet" type="text/css" href="cssc-default.css" />
-<link rel="stylesheet" type="text/css" href="cssc-common.css" />
-<link rel="stylesheet" type="text/css" href="cssc-content.css" />
-' >> ../inc/tpl/header/head.php;
+echo '<link rel="stylesheet" type="text/css" href="assets/css/cssc-default.css" />
+<link rel="stylesheet" type="text/css" href="assets/css/cssc-common.css" />
+<link rel="stylesheet" type="text/css" href="assets/css/cssc-content.css" />
+' >> $MAINDIR"inc/tpl/header/head.php";
 
 # Installation de modules CSS au choix
 read -p "- Utiliser des modules CSSCommon (y/n) ? " use_csscommon
@@ -110,9 +117,8 @@ if [[ $use_csscommon == 'y' ]]; then
         case "$choice" in
             y|O )
                 echo '-- Installation de '$i;
-                pwd;
                 cp CSSCommon/css/cssc-$i.css css/cssc-$i.css
-                echo '<link rel="stylesheet" type="text/css" href="cssc-'$i'.css" />' >> ../inc/tpl/header/head.php;
+                echo '<link rel="stylesheet" type="text/css" href="assets/css/cssc-'$i'.css" />' >> $MAINDIR"inc/tpl/header/head.php";
             ;;
             * );;
         esac
@@ -122,7 +128,7 @@ fi
 # On supprime CSSCommon
 rm -rf CSSCommon
 
-cd ..
+cd $MAINDIR;
 
 #################################################################
 ## GESTION DU JS
@@ -138,28 +144,34 @@ read -p "- Utiliser Mootools, jQuery, ou aucune librairie (m/j/n) ? " choice
 case "$choice" in
     m|M )
         echo '- Installation de MooTools';
+        mkdir jquery;
+        cd jquery;
+        mkdir classes;
         curl -O http://ajax.googleapis.com/ajax/libs/mootools/1.4/mootools-yui-compressed.js;
         if test -f mootools-yui-compressed.js; then
-            echo '<script src="assets/js/mootools-yui-compressed.js"></script><script src="assets/js/events.js"></script>' >> ../../inc/tpl/header/head.php;
-            echo "window.addEvent('domready',function(){});" > events.js;
+            echo '<script src="assets/js/mootools/mootools-yui-compressed.js"></script><script src="assets/js/events.js"></script>' >> $MAINDIR"inc/tpl/header/head.php";
+            echo "window.addEvent('domready',function(){});" > $MAINDIR"assets/js/events.js";
         fi
     ;;
     j|J )
         echo '- Installation de jQuery';
+        mkdir jquery;
+        cd jquery;
+        mkdir plugins;
         curl -O http://code.jquery.com/jquery.min.js;
         if test -f jquery.min.js; then
-            echo '<script src="assets/js/jquery.min.js"></script><script src="assets/js/events.js"></script>' >> ../../inc/tpl/header/head.php;
-            echo "jQuery(document).ready(function($) {});" > events.js;
+            echo '<script src="assets/js/jquery/jquery.min.js"></script><script src="assets/js/events.js"></script>' >> $MAINDIR"inc/tpl/header/head.php";
+            echo "jQuery(document).ready(function($) {});" > $MAINDIR"assets/js/events.js";
         fi
     ;;
     * )
         echo '- Aucune librairie utilisée.';
-        echo '<script src="assets/js/events.js"></script>' >> ../../inc/tpl/header/head.php;
-        echo "(function(){})();" > events.js;
+        echo '<script src="assets/js/events.js"></script>' >> $MAINDIR"inc/tpl/header/head.php";
+        echo "(function(){})();" > $MAINDIR"assets/js/events.js";
     ;;
 esac
 
-cd ../..
+cd $MAINDIR;
 
 #################################################################
 ## COMPATIBILITE IE
@@ -167,14 +179,13 @@ cd ../..
 
 echo '## COMPATIBILITE IE';
 
-cd assets/js/
+cd assets/js/ie/;
 
 # On recupere html5shim
 echo '- Récupération de html5shim ( Tags HTML5 sur IE < 9 )';
 curl -O http://html5shim.googlecode.com/svn/trunk/html5.js
-cd ..
-if test -f js/html5.js; then
-    echo '<!--[if lt IE 9]><script src="assets/js/html5.js"></script><![endif]-->' >> ../inc/tpl/header/head.php
+if test -f html5.js; then
+    echo '<!--[if lt IE 9]><script src="assets/js/html5.js"></script><![endif]-->' >> $MAINDIR"inc/tpl/header/head.php";
 fi
 
 # On recupere selectivizr
@@ -185,11 +196,12 @@ curl -O http://selectivizr.com/downloads/selectivizr-1.0.2.zip
 unzip selectivizr-1.0.2.zip
 cd ..
 if test -f selectivizr/selectivizr-min.js; then
-    mv selectivizr/selectivizr-min.js js/selectivizr-min.js
-    echo '<!--[if lt IE 9]><script src="assets/js/selectivizr-min.js"></script><![endif]-->' >> ../inc/tpl/header/head.php
+    mv selectivizr/selectivizr-min.js selectivizr-min.js
+    echo '<!--[if lt IE 9]><script src="assets/js/selectivizr-min.js"></script><![endif]-->' >> $MAINDIR"inc/tpl/header/head.php";
 fi
 rm -rf selectivizr/
-cd ..
+
+cd $MAINDIR;
 
 #################################################################
 ## VIEWPORT & RESPONSIVE
@@ -201,15 +213,15 @@ echo '## VIEWPORT & RESPONSIVE';
 read -p "- Est-ce un site responsive (y/n) ? " is_responsive
 case "$is_responsive" in
     y|Y|O|o )
-        sed -i '' 's/width=980/width=device-width/' inc/tpl/header/head.php
-        sed -i '' 's/<body>/<body class="cssc-is-responsive">/' inc/tpl/header.php
+        sed -i '' 's/width=980/width=device-width/' $MAINDIR"inc/tpl/header/head.php";
+        sed -i '' 's/<body>/<body class="cssc-is-responsive">/' $MAINDIR"inc/tpl/header.php";
     ;;
     * )
         read -p "- Quelle est la largeur du contenu sans marges (Default:980) ? " content_width
         if [[ $content_width != '' ]]; then
             content_width_wide=$(( $content_width+40 ));
             echo 'Viewport utilisé : '$content_width_wide;
-            sed -i '' 's/width=980/width='$content_width_wide'/' inc/tpl/header/head.php
+            sed -i '' 's/width=980/width='$content_width_wide'/' $MAINDIR"inc/tpl/header/head.php";
         fi
     ;;
 esac
@@ -219,6 +231,8 @@ esac
 #################################################################
 
 echo '## MENAGE';
+
+cd $MAINDIR;
 
 # Suppression des fichiers inutiles & de développement
 rm -rf .git
