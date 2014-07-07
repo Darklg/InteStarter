01d4a04c7c041d6aad70324cb6a05cffd90d1ac0
Darklg <kev.rocher@gmail.com>
2014-07-07 23:39:21 +0200

    Ask for project id

--- newinte.sh
+++ newinte.sh
@@ -35,6 +35,13 @@ if [[ $project_name == '' ]]; then
 fi;
 echo "define('PROJECT_NAME','"${project_name/\'/’}"');" >> $MAINDIR"inc/config.php";
 
+# On recupere l'ID du projet
+read -p "- Quel est l'ID de ce projet ? (default) " project_id
+if [[ $project_id == '' ]]; then
+    project_id='default';
+fi;
+echo "define('PROJECT_ID','"${project_id/\'/’}"');" >> $MAINDIR"inc/config.php";
+
 # On recupere l'URL du projet
 read -p "- Quelle est l'URL du projet ? " project_url
 echo "define('PROJECT_URL','"${project_url/\'/’}"');" >> $MAINDIR"inc/config.php";
