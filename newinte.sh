1354bf4d37b68e23bc05f1f983c57047d231a14c
Darklg <kev.rocher@gmail.com>
2014-10-28 19:19:16 +0100

    Fix bash syntax in compliance with ShellCheck.net

--- newinte.sh
+++ newinte.sh
@@ -4,7 +4,7 @@
 main_commands="npm grunt git";
 for i in $main_commands
 do
-    command -v $i >/dev/null 2>&1 || { echo >&2 "Vous avez besoin du programme \""$i"\" pour continuer."; exit 1; }
+    command -v "$i" >/dev/null 2>&1 || { echo >&2 "Vous avez besoin du programme \"${i}\" pour continuer."; exit 1; }
 done;
 
 # On clone le repository
@@ -12,8 +12,8 @@ echo '# - RECUPERATION DE INTESTARTER'
 git clone https://github.com/Darklg/InteStarter.git
 
 # Set main directory
-MAINDIR=${PWD}"/";
-DIRECTORY=$MAINDIR"InteStarter/";
+MAINDIR="${PWD}/";
+DIRECTORY="${MAINDIR}InteStarter/";
 
 if [ ! -d "$DIRECTORY" ]; then
     echo 'Le clonage a échoué';
@@ -21,21 +21,21 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 # Avoid .git conflict
-rm -rf $MAINDIR"InteStarter/.git/";
+rm -rf "${MAINDIR}InteStarter/.git/";
 
 #################################################################
 ## Basic steps
 #################################################################
 
-chmod -R 777 $MAINDIR"InteStarter/bin/"
+chmod -R 777 "${MAINDIR}InteStarter/bin/"
 
-. $MAINDIR"InteStarter/bin/config.sh"
-. $MAINDIR"bin/folders.sh"
-. $MAINDIR"bin/css.sh"
-. $MAINDIR"bin/js.sh"
-. $MAINDIR"bin/grunt.sh"
-. $MAINDIR"bin/ie.sh"
-. $MAINDIR"bin/responsive.sh"
+. "${MAINDIR}InteStarter/bin/config.sh"
+. "${MAINDIR}bin/folders.sh"
+. "${MAINDIR}bin/css.sh"
+. "${MAINDIR}bin/js.sh"
+. "${MAINDIR}bin/grunt.sh"
+. "${MAINDIR}bin/ie.sh"
+. "${MAINDIR}bin/responsive.sh"
 
 #################################################################
 ## MENAGE
@@ -43,7 +43,7 @@ chmod -R 777 $MAINDIR"InteStarter/bin/"
 
 echo '## MENAGE';
 
-cd $MAINDIR;
+cd "$MAINDIR";
 
 # Suppression des fichiers inutiles & de développement
 rm -rf files
@@ -52,4 +52,4 @@ rm README.md
 rm newinte.sh
 rm deploy.sh
 
-echo '## LET’S WORK, BABY !'
+echo '## FINI !'
