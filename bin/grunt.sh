3bce72fad46430ecb7237dcebf8cc1bc54dd1572
Darklg <kev.rocher@gmail.com>
2014-11-24 23:32:04 +0100

    Use grunt by default

--- bin/grunt.sh
+++ bin/grunt.sh
@@ -4,9 +4,10 @@
 ## Utiliser Grunt
 #################################################################
 
-if [[ $use_grunt == 'y' ]]; then
+if [[ $use_grunt != 'n' ]]; then
     # Create package.json
     echo "{\"name\": \"${project_id}\",\"version\": \"0.0.0\",\"description\": \"\"}" > "${MAINDIR}package.json";
+
     # Install Grunt & default modules
     npm install --save-dev grunt;
     npm install --save-dev load-grunt-config;
