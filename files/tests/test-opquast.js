284f4aa6f20e2855680ac277af4185f6cfa0d92f
Darklg <kev.rocher@gmail.com>
2015-02-11 22:10:06 +0100

    Tests - Opquast : Call each page individually

--- files/tests/test-opquast.js
+++ files/tests/test-opquast.js
@@ -4,22 +4,21 @@ var opquast = require('lib/opquast');
   Run tests
 ---------------------------------------------------------- */
 
-casper.test.begin("\n### Opquast accessibility tests", function suite(test) {
-    /* Start casper */
-    casper.start();
+/* Get pages */
+var main_files = casper.cli.get("main_files").split(' ');
+for (var i = 0, len = main_files.length; i < len; i++) {
+    casper.test.begin("\n### Opquast accessibility tests on page : " + main_files[i], function suite(test) {
+        /* Start casper */
+        casper.start();
 
-    /* Get pages */
-    var main_files = casper.cli.get("main_files").split(' ');
-    for (var i = 0, len = main_files.length; i < len; i++) {
-        console.log('# TESTING PAGE "' + main_files[i] + '"');
+        /* Test page */
         casper.thenOpen('test-' + main_files[i] + '.html', function() {
-            /* Test page */
             opquast.run_accessibility_first_step(test);
         });
-    }
 
-    casper.run(function() {
-        test.done();
+        casper.run(function() {
+            test.done();
+        });
     });
-});
+}
 
