e2d61d1deb42490d0876dd3bc451ff6a231512e7
Kevin <kevin@rocher-dev.com>
2019-08-05 23:45:03 +0200

    Styleguide - Forms : add buttons & fake selectors

--- inc/tpl/styleguide/forms.php
+++ inc/tpl/styleguide/forms.php
@@ -2,14 +2,26 @@
 <form id="styleguide-forms" action="#" method="post">
     <ul class="cssc-form cssc-form--front-end">
         <li class="box">
-            <label for="test_field">Test</label>
+            <label for="test_field">Input</label>
             <input name="test_field" id="test_field" type="text" placeholder="Test" />
         </li>
-        <li class="box" id="special-form-items">
-            <label for="fakeinputbox-me">
-                <input id="fakeinputbox-me" class="fakeinputbox-me" type="checkbox" name="az" value="" />
-                Fake Input
-            </label>
+        <li class="box">
+            <label for="demo_textarea">Textarea</label>
+            <textarea rows="5" cols="20" name="demo_textarea" id="demo_textarea"></textarea>
+        </li>
+        <li class="box">
+            <input checked class="fake-check-ghost" type="checkbox" name="fake-check-ghost" id="fake-check-ghost1" value="" />
+            <label for="fake-check-ghost1">Fake checkbox</label>
+            <input class="fake-check-ghost" type="checkbox" name="fake-check-ghost" id="fake-check-ghost2" value="" />
+            <label for="fake-check-ghost2">Fake checkbox</label>
+        </li>
+        <li class="box">
+            <input checked class="fake-radio-ghost" type="radio" name="fake-radio-ghost" id="fake-radio-ghost1" value="" />
+            <label for="fake-radio-ghost1">Fake radio</label>
+            <input class="fake-radio-ghost" type="radio" name="fake-radio-ghost" id="fake-radio-ghost2" value="" />
+            <label for="fake-radio-ghost2">Fake radio</label>
+        </li>
+        <li class="box">
             <label for="fake-select">Fake select</label>
             <select id="fake-select" name="fake-select" class="fake-select">
                 <option value="" disabled selected style="display:none;">Select</option>
@@ -18,7 +30,7 @@
             </select>
         </li>
         <li class="box">
-            <button class="cssc-button cssc-button--front-end" type="submit">Button</button>
+            <button class="cssc-button cssc-button--front-end" type="submit"><span>Button</span></button>
         </li>
     </ul>
 </form>
