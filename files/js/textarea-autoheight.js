43a83cf323cf386922b1d5f140ecad113efff435
Kevin <kevin@rocher-dev.com>
2021-05-01 22:07:06 +0200

    JS - Textarea autoheight behavior
    - Full JS
    - More events.

---
+++ files/js/textarea-autoheight.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    'use strict';
+
+    function set_textarea_autoheight($item) {
+        $item.style.height = 'auto';
+        $item.style.overflow = 'hidden';
+        $item.style.height = $item.scrollHeight + 'px';
+    }
+
+    ['keyup', 'paste', 'blur', 'input', 'textInput'].forEach(function(ev) {
+        document.body.addEventListener(ev, function(e) {
+            if (e.target.tagName == 'TEXTAREA') {
+                set_textarea_autoheight(e.target);
+            }
+        }, 1);
+    });
+
+    document.querySelectorAll("textarea").forEach(set_textarea_autoheight);
+});
