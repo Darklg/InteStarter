a5e24f9bc640e6b5edf7fcb4099c35782709b117
Kevin <kevin@rocher-dev.com>
2021-05-27 22:48:58 +0200

    CSSC Modals - glowup

--- files/js/modals.js
+++ files/js/modals.js
@@ -6,29 +6,76 @@ jQuery(document).ready(function($) {
         $jQbody.append(jQuery(this));
     });
 
-    /* Open iframe on click */
-    jQuery('[data-modal]').on('click', function(e) {
+    /* Open modal on click */
+    $jQbody.on('click', '[data-modal]', function(e) {
         e.preventDefault();
         var $this = jQuery(this),
             $modal = jQuery('#' + $this.attr('data-modal'));
-        if ($modal.length) {
-            var $iframe = $modal.find('iframe');
-            if ($iframe) {
-                $iframe.attr('src', $iframe.attr('data-src'));
-            }
-            $modal.addClass('is-open');
+        if (!$modal.length) {
+            return;
         }
+        /* Load iframe */
+        var $iframe = $modal.find('iframe[data-src]');
+        if ($iframe.length) {
+            $iframe.attr('src', $iframe.attr('data-src'));
+        }
+        /* Open modal */
+        $modal.addClass('is-open');
     });
 
-    /* Close iframe */
+    /* Close modal */
     $jQbody.on('click', '.modal-overlay,.modal-close', function(e) {
         e.preventDefault();
         jQuery(this).closest('.modal-wrapper').removeClass('is-open');
     });
+
+    /* Close modal on escape */
     jQuery(document).keyup(function(e) {
         if (e.key === "Escape") {
             jQuery('.modal-wrapper').removeClass('is-open');
         }
     });
 
+    $jQbody.trigger('modaldomready');
+
 });
+
+jQuery('body').on('modaldomready', function() {
+
+    /* Build all modals */
+    jQuery('[data-build-modal]').each(function() {
+        build_modal(jQuery(this));
+    });
+
+    /* Open autoloaded modals */
+    jQuery('.modal-wrapper[data-init-modal="1"]').each(function() {
+        jQuery(this)
+            .addClass('is-open')
+            .removeAttr('data-init-modal');
+    });
+});
+
+function build_modal($item) {
+
+    /* Create elements */
+    var $wrapper = jQuery('<div class="modal-wrapper" id="' + $item.attr('data-build-modal') + '"></div>');
+    var $overlay = jQuery('<div class="modal-overlay"></div>');
+    var $inner = jQuery('<div class="modal-inner"></div>');
+    var $close = jQuery('<a href="#" class="modal-close"><span>&times;</span></a>');
+
+    /* Set attributes */
+    $item.removeAttr('data-build-modal');
+    if ($item.attr('data-init-modal') == '1') {
+        $item.removeAttr('data-init-modal');
+        $wrapper.attr('data-init-modal', '1');
+    }
+
+    /* Build modal */
+    $inner.append($close);
+    $inner.append($item);
+    $wrapper.append($overlay);
+    $wrapper.append($inner);
+
+    /* Insert at body end */
+    jQuery('body').append($wrapper);
+}
