05e3b7a9e01ea622dcb06d6a623e2985526c44b3
Kevin <kevin@rocher-dev.com>
2021-06-25 22:49:18 +0200

    JS - better behavior for scroll percent

--- files/js/scroll-percent.js
+++ files/js/scroll-percent.js
@@ -3,10 +3,11 @@
     function setScrollPercent() {
         /* Update max top when scrolling to account for element changing height */
         var _maxTop = document.documentElement.offsetHeight - window.innerHeight,
-            _percentScroll = window.scrollY / _maxTop;
+            _percentScroll = window.scrollY / Math.max(_maxTop, 1);
         document.documentElement.style.setProperty('--scroll-percent', _percentScroll);
     }
     window.addEventListener('scroll', setScrollPercent);
+    window.addEventListener('resize', setScrollPercent);
     window.addEventListener('DOMContentLoaded', setScrollPercent);
     window.addEventListener('load', setScrollPercent);
     setScrollPercent();
