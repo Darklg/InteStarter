d2221f62d98dfc092f8711aafa0c37773c6bbeb2
Kevin <kevin@rocher-dev.com>
2023-08-26 23:27:05 +0200

    Update - Handle CSS Common & Gulp

--- bin/update.sh
+++ bin/update.sh
@@ -10,3 +10,43 @@ if [[ ! -f "${MAINDIR}.jshintrc" ]];then
     echo '- Add missing jshintrc';
     mv "${MAINDIR}files/base.jshintrc" "${MAINDIR}.jshintrc";
 fi;
+
+###################################
+## CSS Common
+###################################
+
+if [[ -f "${MAINDIR}src/scss/csscommon/.git" ]];then
+    echo "- Update CSSCommon submodule";
+    cd "${MAINDIR}src/scss/csscommon/";
+    git pull origin master;
+fi;
+
+###################################
+## Gulp
+###################################
+
+if [[ ! -d "${MAINDIR}src/gulp" ]];then
+    mkdir "${MAINDIR}src/gulp";
+fi;
+if [[ -d "${MAINDIR}gulp" ]];then
+    echo "- Deleted old gulp dir";
+    rm -rf "${MAINDIR}gulp";
+fi;
+if [[ -f "${MAINDIR}src/gulp/intestarter_gulpfile/.git" ]];then
+    echo "- Update intestarter_gulpfile submodule";
+    cd "${MAINDIR}src/gulp/intestarter_gulpfile/";
+    git pull origin main;
+else
+    if [ $(git rev-parse --is-inside-work-tree) ]; then
+        echo "- Add intestarter_gulpfile submodule";
+        cd "${MAINDIR}src/gulp";
+        git submodule add --force https://github.com/Darklg/intestarter_gulpfile.git;
+    fi;
+fi;
+cd "${MAINDIR}";
+
+if [[ -f "${MAINDIR}gulpfile.js" ]];then
+    echo "- Update gulpfile";
+    rm  "${MAINDIR}gulpfile.js";
+    mv "${MAINDIR}files/gulpfile.js" "${MAINDIR}gulpfile.js";
+fi;
