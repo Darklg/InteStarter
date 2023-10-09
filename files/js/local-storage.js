eebeb336df7e1d606359e5f2934323cdbc54647d
Kevin <kevin@rocher-dev.com>
2023-10-09 17:26:21 +0200

    Local storage : fix for set value

--- files/js/local-storage.js
+++ files/js/local-storage.js
@@ -8,14 +8,30 @@ document.addEventListener("DOMContentLoaded", function() {
         var el_key = 'data_jsu_' + el.getAttribute('name');
 
         /* Initial value */
-        var initial_value = localStorage.getItem(el_key);
-        if(initial_value){
-            el.value = initial_value;
-        }
+        set_value(el, localStorage.getItem(el_key));
 
         /* Watch changes */
         el.addEventListener('change', function() {
             localStorage.setItem(el_key, el.value);
         });
     });
+
+    function set_value(el, initial_value) {
+        if (!initial_value) {
+            return;
+        }
+        if (el.tagName == 'SELECT' && !option_exists(el, initial_value)) {
+            return;
+        }
+        el.value = initial_value;
+    }
+
+    function option_exists($select, value) {
+        for (var i = 0; i < $select.options.length; i++) {
+            if ($select.options[i].value == value) {
+                return true;
+            }
+        }
+        return false;
+    }
 });
